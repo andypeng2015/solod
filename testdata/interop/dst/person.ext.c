033354ac273a193c3a9aaa9834b908cf1701644d
Anton <m@antonz.org>
2026-03-05 13:07:08 +0500

    testdata/interop: fix formatting

--- testdata/interop/dst/person.ext.c
+++ testdata/interop/dst/person.ext.c
@@ -1,9 +1,10 @@
+#include <inttypes.h>
 #include "person.ext.h"
 
 int64_t account_inc_balance(Account* a, int64_t amount) {
     int64_t balBefore = a->balance;
     uint8_t* flags = a->flags.ptr;
-    printf("name = %s balance = %lld flags[0] = %u\n",
+    printf("name = %s balance = %" PRId64 " flags[0] = %u\n",
            a->name.ptr, balBefore, a->flags.len > 0 ? flags[0] : 0);
     a->balance += amount;
     return balBefore;
