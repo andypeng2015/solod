c72ccab82340c2e324114f99ee2f0b3790b87b46
Anton <m@antonz.org>
2026-03-10 06:33:09 +0500

    so/unicode/utf8

---
+++ testdata/so-utf8/dst/main.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "so/builtin/builtin.h"
+#include "so/unicode/utf8/utf8.h"
+
+// -- Variables and constants --
---
+++ testdata/so-utf8/src/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/nalgeon/solod/so/unicode/utf8"
+
+func main() {
+	var _ = utf8.MaxRune
+}
