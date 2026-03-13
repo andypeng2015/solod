f1536828cc5a289959e00f0ea949e0e701d1830e
Anton <m@antonz.org>
2026-03-14 03:16:47 +0500

    testdata: std/unicode - smoke test

--- testdata/std/unicode/src/main.go
+++ testdata/std/unicode/src/main.go
@@ -3,5 +3,40 @@ package main
 import "github.com/nalgeon/solod/so/unicode"
 
 func main() {
-	var _ = unicode.Digit
+	{
+		// Is.
+		if !unicode.IsDigit('0') {
+			panic("IsDigit failed")
+		}
+		if !unicode.IsLetter('a') {
+			panic("IsLetter failed")
+		}
+		if !unicode.IsLower('a') {
+			panic("IsLower failed")
+		}
+		if !unicode.IsSpace(' ') {
+			panic("IsSpace failed")
+		}
+		if !unicode.IsTitle('ᾭ') {
+			panic("IsTitle failed")
+		}
+		if !unicode.IsUpper('A') {
+			panic("IsUpper failed")
+		}
+	}
+	{
+		// To.
+		if unicode.ToLower('A') != 'a' {
+			panic("ToLower failed")
+		}
+		if unicode.ToTitle('a') != 'A' {
+			panic("ToTitle failed")
+		}
+		if unicode.ToUpper('a') != 'A' {
+			panic("ToUpper failed")
+		}
+		if unicode.To(unicode.UpperCase, 'a') != 'A' {
+			panic("To failed")
+		}
+	}
 }
